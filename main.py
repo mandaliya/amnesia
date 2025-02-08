6a9f46cc7069f414c7cff57ca8d1acfae7c01997
mandaliya <mandaliya@gmail.com>
2025-02-08 23:31:00 +0000

    Fixing Error

--- main.py
+++ main.py
@@ -38,4 +38,4 @@ def anonymize():
     return jsonify(result.to_dict(orient='records'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
